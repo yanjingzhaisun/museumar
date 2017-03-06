85263a1de9a2c6d0e6bc1424c9a73fa5a88572e6
Euphonux <zz1322@nyu.edu>
2017-03-06 17:39:12 -0500

    first ar test successful.

---
+++ Assets/Scripts/AutoRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AutoRotation : MonoBehaviour {
+	Tween tween;
+
+	void Awake() { 
+		tween = transform.DORotate(new Vector3(0, 360f, 0), 10f, RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);
+		tween.Pause();
+	}
+	void OnEnable() {
+		tween.Play();
+	}
+
+	void OnDisable() {
+		tween.Pause();
+	}
+
+}
---
+++ Assets/Scripts/EarthFadeIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class EarthFadeIn : MonoBehaviour, ITrackableEventHandler {
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+	{
+		Debug.Log("<b>EarthFadeIn</b>:Status changed!");
+		if (newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			Camera.main.transform.Find("BackgroundPlane").gameObject.SetActive(false);
+			Camera.main.clearFlags = CameraClearFlags.Skybox;
+
+		}
+		else {
+			
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		TrackableBehaviour track =  GetComponent<TrackableBehaviour>();
+		if (track != null) {
+			track.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
