309e2e27f0f251f4b2ebff8bdf036145effa50c4
Euphonux <zz1322@nyu.edu>
2017-03-31 11:28:00 -0400

    new version

--- Assets/Scripts/AutoRotation.cs
+++ Assets/Scripts/AutoRotation.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 using DG.Tweening;
 
 public class AutoRotation : MonoBehaviour {
-	Tween tween;
 
-	void Awake() { 
-		tween = transform.DORotate(new Vector3(0, 360f, 0), 10f, RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);
-		tween.Pause();
-	}
-	void OnEnable() {
-		tween.Play();
-	}
 
-	void OnDisable() {
-		tween.Pause();
-	}
+	public float speed;
+	public bool Paused;
 
+	Tween spiningTween;
+
+	// Use this for initialization
+	void Start () {
+		spiningTween = transform.DORotate(new Vector3(0, 180f, 0), 3f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+		spiningTween.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!Paused)
+			spiningTween.Play();
+		else
+			spiningTween.Pause();
+	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+
+	static GameManager _instance = null;
+	public static GameManager instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.Find("GameManager").GetComponent<GameManager>();
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			return _instance;
+		}
+	}
+
+	public Tween periodIndicatorTweener;
+
+	void Awake()
+	{
+		if (GameManager.instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void ChangeTimeSlider(float sliderValue)
+	{
+		if (sliderValue < 0.33f)
+		{
+			ShowTimeEra("Triassic");
+		}
+		else if (sliderValue > 0.33f && sliderValue < 0.67f)
+		{
+			ShowTimeEra("Jurassic");
+		}
+		else if (sliderValue > 0.67f)
+		{
+			ShowTimeEra("Cretaceous");
+		}
+	}
+	void ShowTimeEra(string v)
+	{
+		if (periodIndicatorTweener != null)
+			periodIndicatorTweener.Pause();
+		UnityEngine.UI.Text text = GameObject.Find("PeriodIndicator").GetComponent<UnityEngine.UI.Text>();
+		text.text = v;
+		CanvasGroup cv = text.GetComponent<CanvasGroup>();
+		cv.alpha = 1;
+		periodIndicatorTweener = cv.DOFade(0, 10f);
+		periodIndicatorTweener.Restart();
+	}
+}
+
+
+
